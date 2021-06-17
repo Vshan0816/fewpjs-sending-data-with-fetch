7807275eb531355f9e3455ae1aecf5e4a1a3c18a
Vincent <vshan0816@gmail.com>
2021-06-17 16:51:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name,email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    };
+    fetch("http://localhost:3000/users", configObj) 
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        document.body.append(data[0].id)
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        document.body.append(error.message);
+      })
+
+}
